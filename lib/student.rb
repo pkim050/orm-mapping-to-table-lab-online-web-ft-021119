05b1a1b5e42e4473265d89af5e151208d189adcd
Patrick Kim <pkim050@gmail.com>
2019-03-21 15:39:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,4 +35,8 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT id FROM students WHERE name = ?", self.name).flatten[0]
   end
+
+  def self.create(name:, grade:)
+    
+  end
 end
