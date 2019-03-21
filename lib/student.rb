91913fba14a854bbc700da519cfc107cc8ad3cb4
Patrick Kim <pkim050@gmail.com>
2019-03-21 15:18:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,14 +2,12 @@ require 'pry'
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  @@all = []
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name, grade, id: nil)
     @name = name
     @grade = grade
     @id = id
-    @@all << self
   end
 
   def self.create_table
