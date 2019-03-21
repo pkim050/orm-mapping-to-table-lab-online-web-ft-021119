3e5da8f0ee5e0ebd3b7af795397e846ee617b75e
Patrick Kim <pkim050@gmail.com>
2019-03-21 14:35:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,4 +11,14 @@ class Student
     @@all << self
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
 end
