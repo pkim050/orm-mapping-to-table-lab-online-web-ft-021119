4016a3583e280cf6d080cfed160bb352aa5f6090
Patrick Kim <pkim050@gmail.com>
2019-03-21 14:59:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,8 +36,8 @@ class Student
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
     id = DB[:conn].execute("SELECT id FROM students WHERE name = ?", self.name).flatten[0]
-    #binding.pry
     temp = Student.new(self.name, self.grade, id: id)
     temp
+    binding.pry
   end
 end
